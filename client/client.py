242e358e768fa5b67528ce1bdd037dd222871f38
gaowanlu <2209120827@qq.com>
2022-10-15 11:28:46 +0800

    adding schema pdf

--- client/client.py
+++ client/client.py
@@ -2,22 +2,22 @@ import socket
 import struct
 import time
 
-
 def send_data(client, data):
     cmd = 1
     data_len = len(data)
     data = struct.pack(f'8sII{data_len}s', b'work', cmd, data_len, data.encode('utf-8'))
     client.send(data)  # 发送TCP数据
     info = client.recv(1024).decode()
-    #print(info)
+    print(len(info))
 
 
 if __name__ == '__main__':
     data=''
     for i in range(10):
         data=data+'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabhdsbjchbfvjbdfjvbfjdhvbjhfbh'
+    index = 0 
 
-    host = '127.0.0.1'
+    host = '61.171.51.135'
     port = 2022
     start_time = time.time()
     for index in range(10000):
@@ -26,6 +26,8 @@ if __name__ == '__main__':
         print('连接成功')
         send_data(client, data)
         client.close()
+        print(index)
+        index+=1
     end_time=time.time()
     print('time count: ')
     print((end_time-start_time))
