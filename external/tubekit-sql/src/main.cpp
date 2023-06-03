f47036ba9d74ac4844e2ade0c52472b05533e229
gaowanlu <2209120827@qq.com>
2023-06-03 13:12:49 +0800

    fix: tubekit-sql fixed

--- external/tubekit-sql/src/main.cpp
+++ external/tubekit-sql/src/main.cpp
@@ -11,7 +11,7 @@ using std::shared_ptr;
 int main(int argc, char **argv)
 {
     shared_ptr<connection> conn = std::make_shared<connection>();
-    bool result = conn->connect("", "root", "", "");
+    bool result = conn->connect("", "root", "", "cloudalbum");
     if (result)
     {
         std::cout << "connect success" << std::endl;
@@ -20,11 +20,36 @@ int main(int argc, char **argv)
     m_query.select("SELECT * FROM user");
     std::cout << m_query.update("UPDATE user SET user_age = 66 WHERE user_id = 1 ") << std::endl;
     m_query.select("SELECT * FROM user");
+    {
+        sql<1> m_sql(conn, "SELECT * FROM user WHERE user_id = ?");
+        int64_t param_id = 4;
+        m_sql.set_bind(0, &param_id, sizeof(param_id), value_type::LONGLONG);
 
-    sql<1> m_sql(conn, "SELECT * FROM user WHERE user_id = ?");
-    int id = 1;
-    m_sql.set_bind(0, &id, sizeof(id), value_type::LONG);
-    m_sql.execute(); // BUG
+        tubekit::sql::result<8> m_result;
+        int64_t id;
+        m_result.set_bind(0, &id, sizeof(id), value_type::LONGLONG);
+        int32_t age;
+        m_result.set_bind(1, &age, sizeof(age), value_type::LONG);
+        int64_t avatar;
+        m_result.set_bind(2, &avatar, sizeof(avatar), value_type::LONGLONG);
+        char email[300];
+        m_result.set_bind(3, email, 300, value_type::VAR_STRING);
+        char name[300];
+        m_result.set_bind(4, name, 300, value_type::VAR_STRING);
+        char password[300];
+        m_result.set_bind(5, password, 300, value_type::VAR_STRING);
+        char profile[300];
+        m_result.set_bind(6, profile, 300, value_type::VAR_STRING);
+        int32_t space;
+        m_result.set_bind(7, &space, sizeof(space), value_type::LONG);
 
+        if (m_sql.execute(m_result))
+        {
+            while (m_sql.fetch())
+            {
+                std::cout << id << " " << age << " " << avatar << " " << email << " " << name << " " << password << " " << profile << " " << space << std::endl;
+            }
+        }
+    }
     return 0;
 }
--- external/tubekit-sql/src/sql.h
+++ external/tubekit-sql/src/sql.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <cstring>
 #include <memory>
 #include <mysql/mysql.h>
 #include "connection.h"
@@ -10,26 +11,210 @@ namespace tubekit
     {
         enum value_type
         {
-            TINY = 0,
-            SHORT,
-            LONG,
-            LONGLONG,
-            FLOAT,
-            DOUBLE,
+            BIT = 0,
+            BLOB,
             DATE,
-            TIME,
+            DATETIME2,
             DATETIME,
+            DECIMAL,
+            DOUBLE,
+            ENUM,
+            FLOAT,
+            GEOMETRY,
+            INT24,
+            INVALID,
+            JSON,
+            LONG,
+            LONG_BLOB,
+            LONGLONG,
+            MEDIUM_BLOB,
+            NEWDATE,
+            NEWDECIMAL,
+            NULL_TYPE,
+            SET,
+            SHORT,
+            STRING,
+            TIME2,
+            TIME,
+            TIMESTAMP2,
             TIMESTAMP,
+            TINY,
+            TINY_BLOB,
+            TYPED_ARRAY,
             VAR_STRING,
-            BLOB
+            VARCHAR,
+            YEAR
         };
+
+        /**
+         * @brief query result template
+         *
+         * @tparam bind_size
+         */
+        template <size_t bind_size>
+        class result
+        {
+        public:
+            result();
+            ~result();
+            void set_bind(size_t index, void *buffer, unsigned long size, value_type type);
+            MYSQL_BIND *get_bind();
+            size_t get_size();
+
+        private:
+            MYSQL_BIND bind[bind_size];
+            size_t size{bind_size};
+        };
+
+        template <size_t bind_size>
+        result<bind_size>::result()
+        {
+            ::memset(bind, 0, sizeof(bind));
+        }
+
+        template <size_t bind_size>
+        result<bind_size>::~result()
+        {
+        }
+
+        template <size_t bind_size>
+        size_t result<bind_size>::get_size()
+        {
+            return size;
+        }
+
+        template <size_t bind_size>
+        MYSQL_BIND *result<bind_size>::get_bind()
+        {
+            return this->bind;
+        }
+
+        template <size_t bind_size>
+        void result<bind_size>::set_bind(size_t index, void *buffer, unsigned long size, value_type type)
+        {
+            switch (type)
+            {
+            case BIT:
+                bind[index].buffer_type = MYSQL_TYPE_BIT;
+                break;
+            case BLOB:
+                bind[index].buffer_type = MYSQL_TYPE_BLOB;
+                break;
+            case DATE:
+                bind[index].buffer_type = MYSQL_TYPE_DATE;
+                break;
+            case DATETIME2:
+                bind[index].buffer_type = MYSQL_TYPE_DATETIME2;
+                break;
+            case DATETIME:
+                bind[index].buffer_type = MYSQL_TYPE_DATETIME;
+                break;
+            case DECIMAL:
+                bind[index].buffer_type = MYSQL_TYPE_DECIMAL;
+                break;
+            case DOUBLE:
+                bind[index].buffer_type = MYSQL_TYPE_DOUBLE;
+                break;
+            case ENUM:
+                bind[index].buffer_type = MYSQL_TYPE_ENUM;
+                break;
+            case FLOAT:
+                bind[index].buffer_type = MYSQL_TYPE_FLOAT;
+                break;
+            case GEOMETRY:
+                bind[index].buffer_type = MYSQL_TYPE_GEOMETRY;
+                break;
+            case INT24:
+                bind[index].buffer_type = MYSQL_TYPE_INT24;
+                break;
+            case INVALID:
+                bind[index].buffer_type = MYSQL_TYPE_INVALID;
+                break;
+            case JSON:
+                bind[index].buffer_type = MYSQL_TYPE_JSON;
+                break;
+            case LONG:
+                bind[index].buffer_type = MYSQL_TYPE_LONG;
+                break;
+            case LONG_BLOB:
+                bind[index].buffer_type = MYSQL_TYPE_LONG_BLOB;
+                break;
+            case LONGLONG:
+                bind[index].buffer_type = MYSQL_TYPE_LONGLONG;
+                break;
+            case MEDIUM_BLOB:
+                bind[index].buffer_type = MYSQL_TYPE_MEDIUM_BLOB;
+                break;
+            case NEWDATE:
+                bind[index].buffer_type = MYSQL_TYPE_NEWDATE;
+                break;
+            case NEWDECIMAL:
+                bind[index].buffer_type = MYSQL_TYPE_NEWDECIMAL;
+                break;
+            case NULL_TYPE:
+                bind[index].buffer_type = MYSQL_TYPE_NULL;
+                break;
+            case SET:
+                bind[index].buffer_type = MYSQL_TYPE_SET;
+                break;
+            case SHORT:
+                bind[index].buffer_type = MYSQL_TYPE_SHORT;
+                break;
+            case STRING:
+                bind[index].buffer_type = MYSQL_TYPE_STRING;
+                break;
+            case TIME2:
+                bind[index].buffer_type = MYSQL_TYPE_TIME2;
+                break;
+            case TIME:
+                bind[index].buffer_type = MYSQL_TYPE_TIME;
+                break;
+            case TIMESTAMP2:
+                bind[index].buffer_type = MYSQL_TYPE_TIMESTAMP2;
+                break;
+            case TIMESTAMP:
+                bind[index].buffer_type = MYSQL_TYPE_TIMESTAMP;
+                break;
+            case TINY:
+                bind[index].buffer_type = MYSQL_TYPE_TINY;
+                break;
+            case TINY_BLOB:
+                bind[index].buffer_type = MYSQL_TYPE_TINY_BLOB;
+                break;
+            case TYPED_ARRAY:
+                bind[index].buffer_type = MYSQL_TYPE_TYPED_ARRAY;
+                break;
+            case VAR_STRING:
+                bind[index].buffer_type = MYSQL_TYPE_VAR_STRING;
+                break;
+            case VARCHAR:
+                bind[index].buffer_type = MYSQL_TYPE_VARCHAR;
+                break;
+            case YEAR:
+                bind[index].buffer_type = MYSQL_TYPE_YEAR;
+                break;
+            default:
+                bind[index].buffer_type = MYSQL_TYPE_BLOB;
+                break;
+            }
+            bind[index].buffer = buffer;
+            bind[index].buffer_length = size;
+        }
+
+        /**
+         * @brief sql template
+         *
+         * @tparam bind_size
+         */
         template <size_t bind_size>
         class sql
         {
         public:
             sql(std::weak_ptr<connection> conn, const std::string &sql);
             ~sql();
-            uint64_t execute();
+            template <size_t result_size>
+            bool execute(result<result_size> &m_result);
+            bool fetch();
             void set_bind(size_t index, void *buffer, size_t size, value_type type, bool is_null = false);
 
         private:
@@ -55,17 +240,17 @@ namespace tubekit
         template <size_t bind_size>
         sql<bind_size>::~sql()
         {
-
             mysql_stmt_close(stmt);
         }
 
         template <size_t bind_size>
-        uint64_t sql<bind_size>::execute()
+        template <size_t result_size>
+        bool sql<bind_size>::execute(result<result_size> &m_result)
         {
             if (m_conn.expired())
             {
                 std::cerr << "sql m_conn. is expired" << std::endl;
-                return 0;
+                return false;
             }
             else
             {
@@ -75,69 +260,165 @@ namespace tubekit
                 if (0 != mysql_stmt_bind_param(stmt, bind))
                 {
                     std::cout << mysql_stmt_error(stmt) << std::endl;
+                    return false;
                 }
                 std::cout << "mysql_stmt_execute(stmt)" << std::endl;
-                if (0 != mysql_stmt_execute(stmt)) // BUG
+                if (0 != mysql_stmt_execute(stmt))
                 {
                     std::cout << mysql_stmt_error(stmt) << std::endl;
+                    return false;
                 }
-                std::cout << "execute return" << std::endl;
-                return mysql_affected_rows(m_conn_ptr->get());
+                std::cout << "mysql_stmt_bind_result" << std::endl;
+                if (0 != mysql_stmt_bind_result(stmt, m_result.get_bind()))
+                {
+                    std::cout << mysql_stmt_error(stmt) << std::endl;
+                    return false;
+                }
             }
+            return true;
+        }
+
+        template <size_t bind_size>
+        bool sql<bind_size>::fetch()
+        {
+            int res = mysql_stmt_fetch(stmt);
+            if (res == 0)
+            {
+                return true;
+            }
+            else if (res == 1)
+            {
+                std::cout << "fetch end" << std::endl;
+                return false;
+            }
+            else if (res == MYSQL_NO_DATA)
+            {
+                std::cout << "fetch result is no data" << std::endl;
+                return false;
+            }
+            else if (res == MYSQL_DATA_TRUNCATED)
+            {
+                std::cout << "fetch result is MYSQL_DATA_TRUNCATED" << std::endl;
+                return false;
+            }
+            return false;
         }
 
         template <size_t bind_size>
         void sql<bind_size>::set_bind(size_t index, void *buffer, unsigned long size, value_type type, bool is_null)
         {
-            std::cout << "set_bind size = " << size << std::endl;
             switch (type)
             {
-            case TINY:
-                bind[index].buffer_type = MYSQL_TYPE_TINY;
-                break;
-            case SHORT:
-                bind[index].buffer_type = MYSQL_TYPE_SHORT;
-                break;
-            case LONG:
-                bind[index].buffer_type = MYSQL_TYPE_LONG;
-                break;
-            case LONGLONG:
-                bind[index].buffer_type = MYSQL_TYPE_LONGLONG;
-                break;
-            case FLOAT:
-                bind[index].buffer_type = MYSQL_TYPE_FLOAT;
-                break;
-            case DOUBLE:
-                bind[index].buffer_type = MYSQL_TYPE_DOUBLE;
-                break;
-            case DATE:
-                bind[index].buffer_type = MYSQL_TYPE_DATE;
-                break;
-            case DATETIME:
-                bind[index].buffer_type = MYSQL_TYPE_DATETIME;
-                break;
-            case TIME:
-                bind[index].buffer_type = MYSQL_TYPE_TIME;
-                break;
-            case TIMESTAMP:
-                bind[index].buffer_type = MYSQL_TYPE_TIMESTAMP;
-                break;
-            case VAR_STRING:
-                bind[index].buffer_type = MYSQL_TYPE_VAR_STRING;
+            case BIT:
+                bind[index].buffer_type = MYSQL_TYPE_BIT;
                 break;
             case BLOB:
                 bind[index].buffer_type = MYSQL_TYPE_BLOB;
                 break;
+            case DATE:
+                bind[index].buffer_type = MYSQL_TYPE_DATE;
+                break;
+            case DATETIME2:
+                bind[index].buffer_type = MYSQL_TYPE_DATETIME2;
+                break;
+            case DATETIME:
+                bind[index].buffer_type = MYSQL_TYPE_DATETIME;
+                break;
+            case DECIMAL:
+                bind[index].buffer_type = MYSQL_TYPE_DECIMAL;
+                break;
+            case DOUBLE:
+                bind[index].buffer_type = MYSQL_TYPE_DOUBLE;
+                break;
+            case ENUM:
+                bind[index].buffer_type = MYSQL_TYPE_ENUM;
+                break;
+            case FLOAT:
+                bind[index].buffer_type = MYSQL_TYPE_FLOAT;
+                break;
+            case GEOMETRY:
+                bind[index].buffer_type = MYSQL_TYPE_GEOMETRY;
+                break;
+            case INT24:
+                bind[index].buffer_type = MYSQL_TYPE_INT24;
+                break;
+            case INVALID:
+                bind[index].buffer_type = MYSQL_TYPE_INVALID;
+                break;
+            case JSON:
+                bind[index].buffer_type = MYSQL_TYPE_JSON;
+                break;
+            case LONG:
+                bind[index].buffer_type = MYSQL_TYPE_LONG;
+                break;
+            case LONG_BLOB:
+                bind[index].buffer_type = MYSQL_TYPE_LONG_BLOB;
+                break;
+            case LONGLONG:
+                bind[index].buffer_type = MYSQL_TYPE_LONGLONG;
+                break;
+            case MEDIUM_BLOB:
+                bind[index].buffer_type = MYSQL_TYPE_MEDIUM_BLOB;
+                break;
+            case NEWDATE:
+                bind[index].buffer_type = MYSQL_TYPE_NEWDATE;
+                break;
+            case NEWDECIMAL:
+                bind[index].buffer_type = MYSQL_TYPE_NEWDECIMAL;
+                break;
+            case NULL_TYPE:
+                bind[index].buffer_type = MYSQL_TYPE_NULL;
+                break;
+            case SET:
+                bind[index].buffer_type = MYSQL_TYPE_SET;
+                break;
+            case SHORT:
+                bind[index].buffer_type = MYSQL_TYPE_SHORT;
+                break;
+            case STRING:
+                bind[index].buffer_type = MYSQL_TYPE_STRING;
+                break;
+            case TIME2:
+                bind[index].buffer_type = MYSQL_TYPE_TIME2;
+                break;
+            case TIME:
+                bind[index].buffer_type = MYSQL_TYPE_TIME;
+                break;
+            case TIMESTAMP2:
+                bind[index].buffer_type = MYSQL_TYPE_TIMESTAMP2;
+                break;
+            case TIMESTAMP:
+                bind[index].buffer_type = MYSQL_TYPE_TIMESTAMP;
+                break;
+            case TINY:
+                bind[index].buffer_type = MYSQL_TYPE_TINY;
+                break;
+            case TINY_BLOB:
+                bind[index].buffer_type = MYSQL_TYPE_TINY_BLOB;
+                break;
+            case TYPED_ARRAY:
+                bind[index].buffer_type = MYSQL_TYPE_TYPED_ARRAY;
+                break;
+            case VAR_STRING:
+                bind[index].buffer_type = MYSQL_TYPE_VAR_STRING;
+                break;
+            case VARCHAR:
+                bind[index].buffer_type = MYSQL_TYPE_VARCHAR;
+                break;
+            case YEAR:
+                bind[index].buffer_type = MYSQL_TYPE_YEAR;
+                break;
             default:
+                bind[index].buffer_type = MYSQL_TYPE_BLOB;
                 break;
             }
             bind[index].buffer = buffer;
+            static bool static_is_null = true;
             if (is_null)
-                bind[index].is_null = (bool *)0;
+                bind[index].is_null = &static_is_null;
             else
-                bind[index].is_null = (bool *)1;
+                bind[index].is_null = nullptr;
             bind[index].buffer_length = size;
-            bind[index].length = &bind[index].buffer_length;
         }
     }
 }
