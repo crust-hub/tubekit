131c540d69dfc07ee8389a39eefa7ea9293d8821
gaowanlu <2209120827@qq.com>
2023-09-27 03:20:06 +0800

    fix: connection mgr lock with global_player lock, dead lock

--- src/app/stream_app.cpp
+++ src/app/stream_app.cpp
@@ -8,6 +8,7 @@
 #include "thread/mutex.h"
 #include "utility/singleton.h"
 #include "connection/connection_mgr.h"
+#include <vector>
 
 using tubekit::app::stream_app;
 using tubekit::connection::connection_mgr;
@@ -28,15 +29,21 @@ void stream_app::process_connection(tubekit::connection::stream_connection &m_st
     char *tmp_buffer = new char[data_len];
     m_stream_connection.m_recv_buffer.copy_all(tmp_buffer, data_len);
     m_stream_connection.m_recv_buffer.clear();
+
+    std::vector<void *> vec;
     global_player_mutex.lock();
     for (auto socket_ptr : global_player)
     {
         if (socket_ptr != m_stream_connection.get_socket_ptr())
         {
-            bool b_ret = singleton<connection_mgr>::instance()->safe_send(socket_ptr, tmp_buffer, data_len);
+            vec.push_back(socket_ptr);
         }
     }
     global_player_mutex.unlock();
+    for (auto socket_ptr : vec)
+    {
+        bool b_ret = singleton<connection_mgr>::instance()->safe_send(socket_ptr, tmp_buffer, data_len);
+    }
     delete tmp_buffer;
     // m_stream_connection.mark_close();
 }
@@ -47,6 +54,7 @@ void stream_app::on_close_connection(tubekit::connection::stream_connection &m_s
     using tubekit::app::global_player_mutex;
     global_player_mutex.lock();
     global_player.erase(m_stream_connection.get_socket_ptr());
+    LOG_ERROR("player online %d", global_player.size());
     global_player_mutex.unlock();
 }
 
@@ -56,5 +64,6 @@ void stream_app::on_new_connection(tubekit::connection::stream_connection &m_str
     using tubekit::app::global_player_mutex;
     global_player_mutex.lock();
     global_player.insert(m_stream_connection.get_socket_ptr());
+    LOG_ERROR("player online %d", global_player.size());
     global_player_mutex.unlock();
 }
