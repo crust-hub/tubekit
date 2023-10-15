3419cdcd812c3f6db1fa29746f4d141c06682ed5
gaowanlu <2209120827@qq.com>
2023-10-15 20:49:06 +0800

    feat: start to build ecs actor model lib

---
+++ external/tubekit-ecsactor/main.cpp
@@ -0,0 +1,17 @@
+/**
+ * @file ECS Actor Model
+ * @author gaowanlu (2209120827@qq.com)
+ * @brief
+ * @version 0.1
+ * @date 2023-10-15
+ *
+ * @copyright Copyright (c) 2023
+ *
+ */
+#include <iostream>
+using namespace std;
+
+int main(int argc, char **argv)
+{
+    return 0;
+}
