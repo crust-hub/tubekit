c5b10b7ee78a9d7bd2e24300f4467251e58a8a8a
wanlu <2209120827@qq.com>
2024-01-01 04:13:30 +0800

    Update socket_handler.cpp

--- src/socket/socket_handler.cpp
+++ src/socket/socket_handler.cpp
@@ -339,15 +339,9 @@ void socket_handler::handle()
                     else
                     {
                         int ssl_error = SSL_get_error(socket_ptr->get_ssl_instance(), ssl_status);
-                        if (ssl_error == SSL_ERROR_WANT_READ)
+                        if (ssl_error == SSL_ERROR_WANT_READ || ssl_error == SSL_ERROR_WANT_WRITE)
                         {
-                            // LOG_ERROR("SSL_accept SSL_ERROR_WANT_READ");
                             // need more data or space
-                            attach(socket_ptr);
-                        }
-                        else if (ssl_error == SSL_ERROR_WANT_WRITE)
-                        {
-                            // LOG_ERROR("SSL_accept SSL_ERROR_WANT_WRITE");
                             attach(socket_ptr, true);
                         }
                         else
