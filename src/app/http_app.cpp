46771cde0efda27426e232c4cdf5b4615078451f
gaowanlu <2209120827@qq.com>
2024-04-16 23:54:42 +0800

    update: abstract http callback pointer

--- src/app/http_app.cpp
+++ src/app/http_app.cpp
@@ -3,6 +3,7 @@
 #include <vector>
 #include <filesystem>
 #include <tubekit-log/logger.h>
+#include <tuple>
 #include "server/server.h"
 
 #include "utility/mime_type.h"
@@ -68,19 +69,59 @@ int http_app::on_body(tubekit::connection::http_connection &m_http_connection)
     return 0;
 }
 
+struct http_app_reponse
+{
+    enum type
+    {
+        DIR = 0,
+        FD = 1,
+        NONE = 2,
+    };
+
+    void *ptr{nullptr};
+    type ptr_type{NONE};
+
+    typedef std::tuple<std::string, size_t> DIR_TYPE;
+    typedef FILE FD_TYPE;
+
+    inline void destory()
+    {
+        if (ptr && ptr_type == FD)
+        {
+            FD_TYPE *free_ptr = (FD_TYPE *)ptr;
+            ::fclose(free_ptr);
+            ptr = nullptr;
+            return;
+        }
+        else if (ptr && ptr_type == DIR)
+        {
+            DIR_TYPE *free_ptr = (DIR_TYPE *)ptr;
+            delete free_ptr;
+            ptr = nullptr;
+            return;
+        }
+        else if (ptr)
+        {
+            ::free(ptr);
+            ptr = nullptr;
+        }
+    }
+};
+
 void http_app::process_connection(tubekit::connection::http_connection &m_http_connection)
 {
-    m_http_connection.m_send_buffer.set_limit_max(20230);
     // load callback
     m_http_connection.destory_callback = [](http_connection &m_connection) -> void
     {
         if (m_connection.ptr)
         {
-            FILE *file = (FILE *)m_connection.ptr;
-            ::fclose(file);
+            http_app_reponse *reponse_ptr = (http_app_reponse *)m_connection.ptr;
+            reponse_ptr->destory();
+            delete reponse_ptr;
             m_connection.ptr = nullptr;
         }
     };
+
     m_http_connection.process_callback = [](http_connection &connection) -> void
     {
         string url = utility::url::decode(connection.url);
@@ -105,6 +146,15 @@ void http_app::process_connection(tubekit::connection::http_connection &m_http_c
 
         if (fs::exists(t_path) && fs::is_regular_file(t_path))
         {
+            auto response_ptr = new (std::nothrow) http_app_reponse;
+            if (!response_ptr)
+            {
+                connection.set_response_end(true);
+                return;
+            }
+            connection.ptr = response_ptr;
+            response_ptr->ptr_type = http_app_reponse::FD;
+
             std::string mime_type;
             try
             {
@@ -125,9 +175,9 @@ void http_app::process_connection(tubekit::connection::http_connection &m_http_c
             {
                 LOG_ERROR(e.what());
             }
-            connection.ptr = nullptr;
-            connection.ptr = ::fopen(t_path.c_str(), "r");
-            if (connection.ptr == nullptr)
+
+            response_ptr->ptr = ::fopen(t_path.c_str(), "r");
+            if (response_ptr->ptr == nullptr)
             {
                 connection.set_response_end(true);
                 return;
@@ -136,9 +186,10 @@ void http_app::process_connection(tubekit::connection::http_connection &m_http_c
             // and the response must be set response_end to true, then write after write_end_callback will be continuously recalled
             connection.write_end_callback = [](http_connection &m_connection) -> void
             {
-                char buf[20230] = {0};
+                constexpr int buffer_size = 102400;
+                char buf[buffer_size] = {0};
                 int len = 0;
-                len = ::fread(buf, sizeof(char), 20230, (FILE *)m_connection.ptr);
+                len = ::fread(buf, sizeof(char), buffer_size, (http_app_reponse::FD_TYPE *)((http_app_reponse *)m_connection.ptr)->ptr);
                 if (len > 0)
                 {
                     try
@@ -155,16 +206,31 @@ void http_app::process_connection(tubekit::connection::http_connection &m_http_c
                     m_connection.set_response_end(true);
                 }
             };
+            connection.write_end_callback(connection);
             return;
         }
-
-        if (fs::exists(t_path) && fs::is_directory(t_path))
+        else if (fs::exists(t_path) && fs::is_directory(t_path))
         {
-            connection.ptr = nullptr;
-            const char *response = "HTTP/1.1 200 OK\r\nServer: tubekit\r\nContent-Type: text/html; charset=UTF-8\r\n\r\n";
+            auto response_ptr = new (std::nothrow) http_app_reponse;
+            if (!response_ptr)
+            {
+                connection.set_response_end(true);
+                return;
+            }
+            connection.ptr = response_ptr;
+            response_ptr->ptr_type = http_app_reponse::DIR;
+            response_ptr->ptr = new (std::nothrow) http_app_reponse::DIR_TYPE;
+            if (!response_ptr->ptr)
+            {
+                connection.set_response_end(true);
+                return;
+            }
+            auto dir_type_ptr = (http_app_reponse::DIR_TYPE *)response_ptr->ptr;
+
+            const char *response_head = "HTTP/1.1 200 OK\r\nServer: tubekit\r\nContent-Type: text/html; charset=UTF-8\r\n\r\n";
             try
             {
-                connection.m_send_buffer.write(response, strlen(response));
+                connection.m_send_buffer.write(response_head, strlen(response_head));
             }
             catch (const std::runtime_error &e)
             {
@@ -191,29 +257,51 @@ void http_app::process_connection(tubekit::connection::http_connection &m_http_c
             {
                 body += a_tag;
             }
-            string html = html_loader::load(body);
+            std::get<0>(*dir_type_ptr) = html_loader::load(body);
+            std::get<1>(*dir_type_ptr) = 0;
 
+            connection.write_end_callback = [](http_connection &m_connection) -> void
+            {
+                http_app_reponse *response_ptr = (http_app_reponse *)m_connection.ptr;
+                auto dir_type_ptr = (http_app_reponse::DIR_TYPE *)response_ptr->ptr;
+                const char *buffer_ptr = std::get<0>(*dir_type_ptr).c_str();
+                const size_t buffer_size = std::get<0>(*dir_type_ptr).size();
+                size_t &already_size = std::get<1>(*dir_type_ptr);
+
+                if (buffer_size > already_size)
+                {
+                    try
+                    {
+                        size_t need_send = buffer_size - already_size;
+                        need_send = need_send > 102400 ? 102400 : need_send;
+                        m_connection.m_send_buffer.write(buffer_ptr, need_send);
+                        already_size += need_send;
+                    }
+                    catch (const std::runtime_error &e)
+                    {
+                        LOG_ERROR(e.what());
+                    }
+                }
+                else
+                {
+                    m_connection.set_response_end(true);
+                }
+            };
+            connection.write_end_callback(connection);
+            return;
+        }
+        else
+        {
+            const char *response = "HTTP/1.1 404 Not Found\r\nServer: tubekit\r\nContent-Type: text/text; charset=UTF-8\r\n\r\n";
             try
             {
-                connection.m_send_buffer.write(html.c_str(), html.size());
+                connection.m_send_buffer.write(response, strlen(response));
             }
             catch (const std::runtime_error &e)
             {
                 LOG_ERROR(e.what());
             }
             connection.set_response_end(true);
-            return;
         }
-
-        const char *response = "HTTP/1.1 404 Not Found\r\nServer: tubekit\r\nContent-Type: text/text; charset=UTF-8\r\n\r\n";
-        try
-        {
-            connection.m_send_buffer.write(response, strlen(response));
-        }
-        catch (const std::runtime_error &e)
-        {
-            LOG_ERROR(e.what());
-        }
-        connection.set_response_end(true);
     };
 }
